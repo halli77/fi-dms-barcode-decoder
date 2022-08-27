d306149f880d879d1302e6f66496b0d8c837e4d3
Michael Hallmann <halli@gmx.de>
2022-08-27 21:03:00 +0200

    init

---
+++ fi-dms-barcode-decoder/fi_dms_barcode_decoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  fi_dms_barcode_decoderApp.swift
+//  fi-dms-barcode-decoder
+//
+//  Created by Michael Hallmann on 27.08.22.
+//
+
+import SwiftUI
+
+@main
+struct fi_dms_barcode_decoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
